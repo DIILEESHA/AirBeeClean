5eebfe4c11897597aad8b4ab2b821b0f400badd0
DIILEESHA <dileeshanawarathna18@gmail.com>
2025-04-05 18:47:49 +0530

    added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./layout/Layout";
 import Home from "./pages/home/Home";
+import Pricing from "./pages/pricing/Pricing";
+
 import "./App.css";
 
 const App = () => {
@@ -9,6 +11,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
+          <Route path="/pricing" element={<Pricing />} />
         </Route>
       </Routes>
     </Router>
